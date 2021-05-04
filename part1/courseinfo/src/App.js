94d6a3532174b1d6c9c38f6842a00e5e48cea51b
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-04 19:14:20 +0200

    part1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -25,18 +25,24 @@ const Total = (props) => (
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const excercises1 = 10
-    const part2 = 'Using props to pass data'
-    const excercises2 = 7
-    const part3 = 'State of component'
-    const excercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        excercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        excercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        excercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} excercises1={excercises1} excercises2={excercises2} excercises3={excercises3} />
-            <Total excercises1={excercises1} excercises2={excercises2} excercises3={excercises3} />
+            <Content part1={part1.name} part2={part2.name} part3={part3.name} excercises1={part1.excercises} excercises2={part2.excercises} excercises3={part3.excercises} />
+            <Total excercises1={part1.excercises} excercises2={part2.excercises} excercises3={part3.excercises} />
         </div>
     )
 }
