3656dfc4b30aa9ecff098608d430f2f0bfb4e21b
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-06 21:47:04 +0200

    part2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
@@ -12,6 +13,14 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          setPersons(response.data)
+        })
+  },[])
 
   const handleSubmit = (event) => {
     event.preventDefault()
