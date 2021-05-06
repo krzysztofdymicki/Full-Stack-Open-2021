f56576e7a74625f5e2cd5e3464455592c637ce7c
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-06 12:07:32 +0200

    part2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,37 +1,6 @@
 import React from 'react'
+import Course from './components/Course'
 
-const Header = (props) => (
-    <>
-        <h1>{props.course.name}</h1>
-    </>
-)
-
-const Part = (props) => (
-    <p>{props.part.name} {props.part.excercises}</p>
-)
-const Content = (props) => (
-    <div>
-      {props.course.parts.map( p => <Part key={p.id} part={p} />)}
-    </div>
-)
-
-const Total = (props) => (
-    <>
-      <p>Total number of excercises {props.course.parts.reduce((sum, p) => sum += p.excercises, 0)}</p>  
-    </>
-)
-
-const Course = ({ course }) => {
-
-  return (
-    <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-    </div>
-)
-
-}
 
 const App = () => {
     const courses = [
