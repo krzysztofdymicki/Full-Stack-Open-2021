13e28d336bd92eba4e6c6a40b0f1102d75834cb5
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-15 13:09:54 +0200

    part9.5

--- part9a/bmicalculator.ts
+++ part9a/bmicalculator.ts
@@ -3,34 +3,56 @@ interface heightAndWeight {
     weight: number
 }
 
-const parseArgs = (args: Array<string>): heightAndWeight => {
+interface bmi {
+    height: number,
+    weight: number,
+    bmi: string
+}
+
+const parseArgs = (args: Array<string | number >): heightAndWeight => {
 
     if(args.length < 4) throw new Error('You provided not enough arguments')
     if(args.length > 4) throw new Error('You provided too many arguments')
 
-    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    if(!isNaN(Number(args[0])) && !isNaN(Number(args[1]))) {
         return {
             height: Number(args[2]),
             weight: Number(args[3])
         }
     } else {
-        throw new Error ('Proviced arguments were not numbers')
+        throw new Error ('Proviced arguments were not numbers or string')
     }
 }
 
-const bmiCalculator = (h: number, w: number) => {
+const bmiCalculator = (h: number, w: number): bmi => {
 
     const bmi = w*10000/Math.pow(h, 2)
 
     if(bmi < 18.5) {
-        console.log('bmi', bmi, 'underweight')
+        return {
+            height: h,
+            weight: w,
+            bmi: 'underweight'
+        }
     }else if (bmi >= 18.5 && bmi < 25) {
-        console.log('bmi', bmi, 'Normal (healthy weight)')
+        return {
+            height: h,
+            weight: w,
+            bmi: 'Normal (healthy weight)'
+        }
     }else if (bmi >= 25 && bmi <30) {
-        console.log('bmi', bmi, 'overweight')
+        return {
+            height: h,
+            weight: w,
+            bmi: 'overweight' 
+        }
     }else if (bmi >=30) {
-        console.log('bmi', bmi, 'overweight')
-    }
+        return {
+            height: h,
+            weight: w,
+            bmi: 'obese'
+        }
+    }else throw new Error('something gone wrong, probaby you didnt pass arguments height and weight')
 }
 
 try {
@@ -39,3 +61,5 @@ try {
 } catch(e) {
     console.log('Something went wrong', e.message)
 }
+
+export default bmiCalculator
--- part9a/index.ts
+++ part9a/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import bmiCalculator from './bmicalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Fullstack')
+})
+
+app.get('/bmi', (req, res) => {
+    const {height, weight} = req.query
+    const healthState = bmiCalculator(Number(height), Number(weight))
+    res.json(healthState)
 })
 
 const PORT = 3001
