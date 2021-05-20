133f6af16d11bac2a2a0b3677423c7d85dc1d682
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-20 20:42:25 +0200

    part 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (array) => {
   return 1;
 };
 
+const totalLikes = (array) => {
+  return array.length === 0 
+    ? 0
+    : array.reduce((sum, e) => sum + e.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes
 };
