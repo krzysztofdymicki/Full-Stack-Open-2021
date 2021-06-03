1774cb59275e36a9d50d1ccada202d52bd40b1ce
unknown <krzysztof.dymicki.bp@gmail.com>
2021-06-03 15:02:23 +0200

    part6.4

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor, addNew } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.sort((a,b) => b.votes - a.votes))
   const dispatch = useDispatch()
 
   const vote = (id) => {
